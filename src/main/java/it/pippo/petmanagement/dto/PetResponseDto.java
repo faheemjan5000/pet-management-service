7a7dbf74152ca582a3366eb6510b9f72ebf5d54e
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2026-03-22 15:52:16 +0100

    request/response dtos added

---
+++ src/main/java/it/pippo/petmanagement/dto/PetResponseDto.java
@@ -0,0 +1,27 @@
+package it.pippo.petmanagement.dto;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetResponseDto {
+
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Species is mandatory")
+    private String species;
+
+    @Min(value = 0, message = "Age must be greater than or equal to 0")
+    private Integer age;
+
+    private String ownerName;
+}
